c145f8482c7cd671729f242b5aedc758e3200872
Anikushin Roman <anikushin.roma@gmail.com>
2022-12-13 19:43:01 +0700

    Task #16122 сделан набросок сервиса заказов

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -1,6 +1,12 @@
 ﻿using System.Security.Claims;
 using FoodDelivery.Database.Context;
+using FoodDelivery.Exception;
+using FoodDelivery.Models;
 using FoodDelivery.Models.Dto;
+using FoodDelivery.Models.Entity;
+using FoodDelivery.Utils;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace FoodDelivery.Services;
 
@@ -15,21 +21,81 @@ public class OrderService : IOrderService
 
     public OrderDto GetOrderInfo(ClaimsPrincipal principal, Guid orderId)
     {
-        throw new NotImplementedException();
+        var result = (
+            from basketDish in _context.BasketDishes
+            where basketDish.OrderId == orderId && basketDish.UserId == ClaimsUtils.getId(principal)
+            select basketDish
+        );
+
+        throw new NotImplementedException(); // TODO
     }
 
     public List<OrderInfoDto> GetOrderList(ClaimsPrincipal principal)
     {
-        throw new NotImplementedException();
+        var result = (
+            from basketDish in _context.BasketDishes
+            where basketDish.UserId == ClaimsUtils.getId(principal)
+            select basketDish
+        );
+        
+        throw new NotImplementedException(); // TODO
     }
 
     public void CreateOrderFromBasket(ClaimsPrincipal principal, OrderCreateDto orderCreateDto)
     {
-        throw new NotImplementedException();
+        var result = (
+            from basketDish in _context.BasketDishes
+            where basketDish.OrderId == null && basketDish.UserId == ClaimsUtils.getId(principal)
+            select basketDish
+        );
+
+        var price = result.Sum(basketDish => basketDish.Count * basketDish.Dish.Price);
+
+        Order order = new Order
+        {
+            OrderTime = DateTime.Now,
+            DeliveryTime = orderCreateDto.DeliveryTime,
+            Address = orderCreateDto.Address,
+            Price = price,
+            Status = OrderStatus.InProcess
+        };
+
+        _context.Orders.Add(order);
+        foreach (var basketDish in result)
+        {
+            basketDish.Order = order;
+        }
+
+        try
+        {
+            _context.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            if (PostgresUtils.HasErrorCode(e, PostgresErrorCodes.UniqueViolation))
+            {
+                // TODO
+            }
+            
+            if (PostgresUtils.HasErrorCode(e, PostgresErrorCodes.ForeignKeyViolation))
+            {
+                throw new NotFoundException();
+            }
+
+            throw;
+        }
     }
 
     public void ConfirmOrderDelivery(ClaimsPrincipal principal, Guid orderId)
     {
-        throw new NotImplementedException();
+        var order = new Order
+        {
+            Id = orderId
+        };
+
+        _context.Attach(order);
+        order.Status = OrderStatus.Delivered;
+
+        _context.SaveChanges();
     }
 }
