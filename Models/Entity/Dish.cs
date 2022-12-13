03a78734ab6292c36ff21fe198f5e065873bdf61
Anikushin Roman <anikushin.roma@gmail.com>
2022-12-13 18:13:41 +0700

    Task #16119 добавлен набросок сервиса блюд

--- Models/Entity/Dish.cs
+++ Models/Entity/Dish.cs
@@ -7,7 +7,7 @@ public class Dish
     [Key]
     public Guid Id { get; set; }
     
-    public string Email { get; set; }
+    public string Name { get; set; }
     
     public string Description { get; set; }
     
--- Services/DishService.cs
+++ Services/DishService.cs
@@ -1,12 +1,19 @@
 ﻿using System.Security.Claims;
 using FoodDelivery.Database.Context;
+using FoodDelivery.Exception;
 using FoodDelivery.Models;
 using FoodDelivery.Models.Dto;
+using FoodDelivery.Models.Entity;
+using FoodDelivery.Utils;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace FoodDelivery.Services;
 
 public class DishService : IDishService
 {
+    private const int PageSize = 5;
+    
     private readonly FoodDeliveryContext _context;
 
     public DishService(FoodDeliveryContext context)
@@ -16,12 +23,59 @@ public class DishService : IDishService
 
     public DishPagesListDto GetDishPage(IList<DishCategory> categories, bool vegetarian, DishSorting? sorting, int page)
     {
-        throw new NotImplementedException();
+        IEnumerable<Dish> result = (from dish in _context.Dishes
+            where dish.Vegetarian == vegetarian && categories.Contains(dish.Category)
+            orderby dish.Name
+            // TODO offset
+            select dish).Take(PageSize);
+
+        return new DishPagesListDto
+        {
+            Dishes = (from dish in result
+                select new DishDto
+                {
+                    Id = dish.Id,
+                    Name = dish.Name,
+                    Description = dish.Description,
+                    Price = dish.Price,
+                    Image = dish.Image,
+                    Vegetarian = dish.Vegetarian,
+                    Rating = dish.Rating,
+                    Category = dish.Category
+                }).ToList(),
+            Pagination = new PageInfoModel
+            {
+                Size = PageSize,
+                Count = 1, // TODO
+                Current = 1 // TODO
+            }
+        };
     }
 
     public DishDto GetDishInfo(Guid dishId)
     {
-        throw new NotImplementedException();
+        Dish? result = (
+            from dish in _context.Dishes
+            where dish.Id == dishId
+            select dish
+        ).SingleOrDefault();
+
+        if (result == null)
+        {
+            throw new NotFoundException();
+        }
+        
+        return new DishDto
+        {
+            Id = result.Id,
+            Name = result.Name,
+            Description = result.Description,
+            Price = result.Price,
+            Image = result.Image,
+            Vegetarian = result.Vegetarian,
+            Rating = result.Rating,
+            Category = result.Category
+        };
     }
 
     public bool CanSetRating(ClaimsPrincipal principal, Guid dishId)
@@ -31,6 +85,35 @@ public class DishService : IDishService
 
     public void SetRating(ClaimsPrincipal principal, Guid dishId, int rating)
     {
-        throw new NotImplementedException();
+        if (!CanSetRating(principal, dishId))
+        {
+            // TODO throw
+        }
+
+        _context.DishRatings.Add(new DishRating
+        {
+            UserId = ClaimsUtils.getId(principal),
+            DishId = dishId,
+            Rating = rating
+        });
+        
+        try
+        {
+            _context.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            if (PostgresUtils.HasErrorCode(e, PostgresErrorCodes.UniqueViolation))
+            {
+                // TODO
+            }
+            
+            if (PostgresUtils.HasErrorCode(e, PostgresErrorCodes.ForeignKeyViolation))
+            {
+                throw new NotFoundException();
+            }
+
+            throw;
+        }
     }
 }
